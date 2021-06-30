c2a63c553fabf41fbbb287c18f8406a9af0f1e36
GOWREESHAN <81855215+gowree@users.noreply.github.com>
2021-06-30 11:02:06 +0530

    Add files via upload
    Add java script file

---
+++ validate.js
@@ -0,0 +1,13 @@
+function validate()
+{ var fname=document.form.fname.value;
+    var lname=document.form.lname.value;
+    var hd=document.form.hd.value;
+    var pn=document.form.pn.value;
+    var email=document.form.email.value;
+    
+
+if (fname=="" || lname=="" || hd=="" ||pn=="" ||email=="")
+{alert("Some fields are empty, Fill every Fields ");
+    return false;}
+
+}
