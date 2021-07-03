94af0744d4fafc90cd866ee0d3933b1f92969eb0
KA DE SILVA <81987342+kadsilva@users.noreply.github.com>
2021-07-03 17:46:23 +0530

    comment box

--- about.php
+++ about.php
@@ -1,52 +1,21 @@
-<
 <?php
-$uname=$_POST['uname'];
-$service=$_POST['service'];
-$comment=$_POST['comment'];
+$uname=$_POST["uname"];
+$serve=$_POST["service"];
+$comm=$_POST["comment"];
 
 //connection
 
-$con=mysqli_connect("localhost","root","");
+$con=mysqli_connect("localhost","root","","about");
 if($con)
 {
 	echo "connection succesfull";
 }
 else
 {
-	echo "connection fail",mysqli_connect_eror();
+	echo "connection fail",mysqli_connect_error();
 }
 
-//create database
-/*
-
-$db="CREATE DATABASE ABOUT";
-if(mysqli_query($con,$db))
-{
-    echo "Dtabase Created";
-}
-else
-{
-   echo "Dtabase Creating is fail";
-}
-*/
-
-//create a table in database
-
-mysqli_select_db($con,"ABOUT");
-/*
-$TABLE="CREATE TABLE COMMENT(uname varchar(50),how_about_Service varchar(20),comment varchar(180))";
-if(mysqli_query($con,$TABLE))
-{
-    echo "Table is created";
-}
-else
-{
-   echo "Table is not created";
-}
-*/
-
-//inserting values
-$insert="INSERT INTO COMMENT VALUES('uname','service','comment')";
+$insert="INSERT INTO comment VALUES('$uname','$serve','$comm');";
 if(mysqli_query($con,$insert))
 {
     echo "Data inserted successfully";
@@ -59,4 +28,6 @@ else
 
 
 ?>
+
+
 
